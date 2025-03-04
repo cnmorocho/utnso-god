476fa4dce61be4c508b2a7a2c09f4242a392706c
cnmorocho <carlos.nahuel.morocho@gmail.com>
2025-03-04 01:06:42 -0300

    refactor: se corrigio typo

--- src/kernel/entities/process.go
+++ src/kernel/entities/process.go
@@ -3,16 +3,16 @@ package entities
 import "cnmorocho/utnso-god/src/utils"
 
 type Process struct {
-	Id             int
-	Size           int
-	NewTreads      utils.Queue[Thread]
-	ReadyTreads    utils.Queue[Thread]
-	ExecutedTreads utils.Queue[Thread]
-	BlockedTreads  utils.Queue[Thread]
-	ExitedTreads   utils.Queue[Thread]
+	Id              int
+	Size            int
+	NewThreads      utils.Queue[Thread]
+	ReadyThreads    utils.Queue[Thread]
+	ExecutedThreads utils.Queue[Thread]
+	BlockedThreads  utils.Queue[Thread]
+	ExitedThreads   utils.Queue[Thread]
 }
 
 func (p *Process) CreateThread() {
 	tid := utils.GenerateId()
-	p.NewTreads.Enqueue(Thread{Id: tid})
+	p.NewThreads.Enqueue(Thread{Id: tid})
 }
